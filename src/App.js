4106382d123b177e15f0190ce21debe5845e6dc4
Franklyn Tique <franktique@gmail.com>
2020-11-19 09:41:32 -0500

    se agregan librerias como primeract y bootstarp

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Button from './components/button/button';
 
+
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
     <div className="App">
